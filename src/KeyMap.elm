02149dc45ad337fb1a8326db05d3bdccde3fa418
JBetz <joebetz91@gmail.com>
2017-10-15 22:31:48 -0500

    using keyboard extra package to reference keys by name rather than code

--- src/KeyMap.elm
+++ src/KeyMap.elm
@@ -1,7 +1,8 @@
 module KeyMap exposing (..)
 
-import Char exposing (KeyCode)
 import Dict exposing (Dict(..))
+import Keyboard exposing (KeyCode)
+import Keyboard.Extra exposing (Key(..), toCode)
 
 
 type alias KeyMap =
@@ -19,129 +20,19 @@ keyMap : KeyLayout -> KeyMap
 keyMap layout =
     case layout of
         Qwerty ->
-            Dict.fromList [ ( w, 0 ), ( q, 1 ), ( e, 2 ), ( a, 3 ), ( s, 4 ), ( d, 5 ), ( z, 6 ), ( c, 7 ), ( x, 8 ) ]
+            mkMap [ CharW, CharQ, CharE, CharA, CharS, CharD, CharZ, CharC, CharX ]
 
         Dvorak ->
-            Dict.fromList [ ( 188, 0 ), ( 222, 1 ), ( 190, 2 ), ( 65, 3 ), ( 79, 4 ), ( 69, 5 ), ( 59, 6 ), ( 74, 7 ), ( 88, 8 ) ]
+            mkMap [ Comma, Quote, Period, CharA, CharO, CharE, Semicolon, CharJ, CharQ ]
 
         Azerty ->
-            Dict.fromList [ ( 90, 0 ), ( 65, 1 ), ( 69, 2 ), ( 81, 3 ), ( 83, 4 ), ( 68, 5 ), ( 90, 6 ), ( 67, 7 ), ( 88, 8 ) ]
+            mkMap [ CharZ, CharA, CharE, CharQ, CharS, CharD, CharW, CharC, CharX ]
 
         Qwertz ->
-            Dict.fromList [ ( 87, 0 ), ( 81, 1 ), ( 69, 2 ), ( 65, 3 ), ( 83, 4 ), ( 68, 5 ), ( 89, 6 ), ( 67, 7 ), ( 88, 8 ) ]
+            mkMap [ CharW, CharQ, CharE, CharA, CharS, CharD, CharY, CharC, CharX ]
 
 
-a =
-    65
-
-
-b =
-    66
-
-
-c =
-    67
-
-
-d =
-    68
-
-
-e =
-    69
-
-
-f =
-    70
-
-
-g =
-    71
-
-
-h =
-    72
-
-
-i =
-    73
-
-
-j =
-    74
-
-
-k =
-    75
-
-
-l =
-    76
-
-
-m =
-    77
-
-
-n =
-    78
-
-
-o =
-    79
-
-
-p =
-    80
-
-
-q =
-    81
-
-
-r =
-    82
-
-
-s =
-    83
-
-
-t =
-    84
-
-
-u =
-    85
-
-
-v =
-    86
-
-
-w =
-    87
-
-
-x =
-    88
-
-
-y =
-    89
-
-
-z =
-    90
-
-
-shift =
-    16
-
-
-ctrl =
-    17
-
-
-alt =
-    18
+mkMap : List Key -> KeyMap
+mkMap keys =
+    Dict.fromList <|
+        List.map2 (,) (List.map toCode keys) (List.range 0 8)
